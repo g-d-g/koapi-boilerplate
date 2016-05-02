4a8d91becd687d227cb429747312d08b6c0db269
Garbin Huang <garbinh@gmail.com>
2016-05-02 07:24:17 +0000

    * add npm script queue:ui

--- queue.js
+++ queue.js
@@ -13,39 +13,41 @@ program.option('-u --ui', 'UI')
        .option('-p --port <n>', 'Port')
        .parse(process.argv);
 
-Model.init(config.knex);
-if (process.env != 'production'){
-  require('babel-register');
-  queues = './src/queues/*';
-}
-if(cluster.isMaster){
-  for (var i = 0; i < require('os').cpus().length; i++) {
-    cluster.fork();
-  }
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('worker ' + worker.process.pid + ' died');
+if (program.ui && program.port) {
+  var matador = require('bull-ui/app')({
+    redis: {
+      host: config.redis.host,
+      port: config.redis.port,
+      password: config.redis.password
+    }
   });
-  if (program.ui && program.port) {
-    var matador = require('bull-ui/app')({
-      redis: {
-        host: config.redis.host,
-        port: config.redis.port,
-        password: config.redis.password
-      }
-    });
 
-    matador.listen(program.port, function(){
-      console.log('Bull-UI started listening on port', this.address().port);
+  matador.listen(program.port, function(){
+    console.log('Bull-UI started listening on port', this.address().port);
+  });
+} else {
+  Model.init(config.knex);
+  if (process.env != 'production'){
+    require('babel-register');
+    queues = './src/queues/*';
+  }
+  if(cluster.isMaster){
+    for (var i = 0; i < require('os').cpus().length; i++) {
+      cluster.fork();
+    }
+    cluster.on('exit', function(worker, code, signal) {
+      console.log('worker ' + worker.process.pid + ' died');
+    });
+  }else{
+    glob.sync(queues).forEach(function (file, index) {
+      var queue = require(file);
+      queue.default.process(queue.worker);
+      queue.default.on('ready', function(){
+        console.log( 'Queue %s ready for jobs, PID:%s',
+        queue.default.name,
+        cluster.worker.process.pid );
+      });
     });
   }
-}else{
-  glob.sync(queues).forEach(function (file, index) {
-    var queue = require(file);
-    queue.default.process(queue.worker);
-    queue.default.on('ready', function(){
-      console.log( 'Queue %s ready for jobs, PID:%s',
-      queue.default.name,
-      cluster.worker.process.pid );
-    });
-  });
+
 }
