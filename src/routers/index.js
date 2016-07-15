2b90f1a0b554aad0f961ac796aeb0108f5c5ad6f
Garbin Huang <garbinh@gmail.com>
2016-07-15 15:23:50 +0000

    * update koapi => 0.2.20

--- src/routers/index.js
+++ src/routers/index.js
@@ -4,14 +4,14 @@ import Comment from '../models/comment';
 import index from './default'
 import subdomain from './subdomain'
 
-const posts = (new ResourceRouter).resource(Post.collection());
+const posts = (new ResourceRouter(Post.collection())).crud();
 
-const comments = new ResourceRouter;
+const comments = new ResourceRouter(ctx => ctx.state.post.comments());
 comments.use(async (ctx, next)=>{
   ctx.state.post = await Post.where({id:ctx.params.post_id}).fetch({required:true});
   await next()
 });
-comments.resource(ctx => ctx.state.post.comments());
+comments.crud();
 
 export default [
   subdomain,
