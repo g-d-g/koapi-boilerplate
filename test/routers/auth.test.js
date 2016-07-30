db7bdba68e999b3ab1e475ce6ea14b4f058d72ba
Garbin Huang <garbinh@gmail.com>
2016-07-30 14:46:50 +0000

    * clear

--- test/routers/auth.test.js
+++ test/routers/auth.test.js
@@ -3,26 +3,6 @@ import {test, expect, request} from 'koapi/lib/test'
 import nock from 'nock'
 
 
-nock('http://ubuntu:5000')
-.get('/auth/github/callback')
-.query({
-  code: '5b7469b93ec67d18b57f',
-  state: 'eyJjbGllbnRfaWQiOiIxMjMifQ=='
-})
-.reply(200, {});
-
-nock('https://github.com')
-.get('/login/oauth/authorize')
-.query({
-  response_type: 'code',
-  redirect_uri: 'http://ubuntu:5000/auth/github/callback',
-  state: 'eyJjbGllbnRfaWQiOiIxMjMifQ==',
-  client_id:'f2ee0541fecc7c773d5d',
-})
-.reply(302, undefined, {
-  Location: 'http://ubuntu:5000/auth/github/callback?code=5b7469b93ec67d18b57f&state=eyJjbGllbnRfaWQiOiIxMjMifQ%3D%3D'
-});
-
 nock('https://github.com')
 .post('/login/oauth/access_token', {
   grant_type:'authorization_code',
@@ -36,15 +16,23 @@ nock('https://github.com')
   token_type: 'bearer',
 });
 
-test('GET /auth/github', t =>
-  request(server).get('/auth/github?state=eyJjbGllbnRfaWQiOiIxMjMifQ%3D%3D')
-                 .then(res => {
-                   expect(res).to.have.status(200);
-                 })
-);
+nock('https://github.com')
+.get('/login/oauth/authorize')
+.query({
+  response_type: 'code',
+  redirect_uri: 'http://ubuntu:5000/auth/github/callback',
+  state: 'eyJjbGllbnRfaWQiOiIxMjMifQ==',
+  client_id:'f2ee0541fecc7c773d5d',
+})
+.reply(302, undefined, {
+  Location: 'http://127.0.0.1:' + server.address().port + '/auth/github/callback?code=5b7469b93ec67d18b57f&state=eyJjbGllbnRfaWQiOiIxMjMifQ%3D%3D'
+});
 
-test('GET /auth/github/callback', t =>
-  request(server).get('/auth/github/callback?code=5b7469b93ec67d18b57f&state=eyJjbGllbnRfaWQiOiIxMjMifQ%3D%3D')
+
+test('GET /auth/github', t =>
+  // redirect to /auth/github/callback
+  // redirect to /protected
+  request(server).get('/auth/github?state=eyJjbGllbnRfaWQiOiIxMjMifQ%3D%3D')
                  .then(res => {
                    expect(res).to.have.status(200);
                  })
